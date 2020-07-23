c1991df966714d9e63c48f48e83691745b4704fb
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-07-23 14:44:12 +0100

    Storing messages

--- run.py
+++ run.py
@@ -2,7 +2,13 @@ import os
 from os import getenv
 from flask import Flask
 
-app = Flask (__name__)
+app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    """This takes username and message and adds to list"""
+    messages.append("{}: {}".format(username, message))
+
 
 @app.route('/')
 def index():
@@ -12,10 +18,14 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """Display chat message"""
+    return "Welcome, {0}".format(username, messages)
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """Create new message and redirect back to chat page"""
     return "{0}: {1}".format(username, message)
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT', 5000)), debug=True)
